28505aa42844fc4eb9fec0db179b9ded3ca6beb4
profyoni <jrobinson@touro.edu>
2025-10-29 20:36:39 -0400

    Arraylist beginnings

---
+++ tests/MySinglyLinkedListTest.java
@@ -0,0 +1,61 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MySinglyLinkedListTest {
+    private List<String> list = new MySinglyLinkedList();
+    @Test
+    void size() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void add() {
+
+        list.add("a");
+        list.add("b");
+        list.add("c");
+
+        assertEquals(3, list.size());
+    }
+
+    @Test
+    void clear() {
+        list.add("a");
+        list.add("b");
+        assertEquals(2, list.size());
+        list.clear();
+        assertEquals(0, list.size());
+
+    }
+
+    @Test
+    void get() {
+        list.add("a");
+        list.add("b");
+
+        assertEquals("a", list.get(0));
+        assertEquals("b", list.get(1));
+    }
+
+    @Test
+    void set() {
+        list.add("a");
+        list.add("b");
+
+        assertEquals( "a",list.set(0, "c"));
+        list.set(1, "d");
+
+        assertEquals("c", list.get(0));
+        assertEquals("d", list.get(1));
+    }
+}
